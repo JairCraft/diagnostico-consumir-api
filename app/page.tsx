07c9f42fe5fb928c47ca7bdaef480956b4b0b418
JairCraft <71790709+JairCraft@users.noreply.github.com>
2024-11-14 15:18:35 -0500

    perf: :zap: modified the fetching method to improve render

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-"use client"
-
-import useFetch from './useFetch.js'
+import { Suspense } from 'react';
+import fetchData from './fetchData.js'
 import styles from "./page.module.css";
 
-export default function Home() {
+const data = fetchData("https://v2.jokeapi.dev/joke/Any?lang=es&type=single")
 
-  const { data, loading } = useFetch("https://v2.jokeapi.dev/joke/Any?lang=es&type=single")
+export default function Home() {
+  const readData = data.read()
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.ctas}>
-          {loading && <a>Cargando...</a>}
-          <a>
-            La broma es {data?.joke}
-          </a>
+          <Suspense fallback={<div>Cargando...</div>}>
+            <a>
+              La broma es {readData.joke}
+            </a>
+          </Suspense>
         </div>
       </main>
     </div>
